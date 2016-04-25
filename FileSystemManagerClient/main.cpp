42a6785f8c8562e5bce439b364a3ed61822ea912
Ionita Cosmin <ionita.cosmin@outlook.com>
2016-04-25 23:37:52 +0300

    Added initial project files

---
+++ FileSystemManagerClient/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "This will be the client" << endl;
+    return 0;
+}
---
+++ FileSystemManagerServer/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "This project will be the server" << endl;
+    return 0;
+}
